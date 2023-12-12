fdb934d8752a4c1c32adf3438b902d4a5817ef53
scbaek83 <scpenny@hanmail.net>
2023-12-12 09:47:25 +0900

    Modify trackbar.cpp as interactive trackbar from book ex.

--- trackbar.cpp
+++ trackbar.cpp
@@ -1,56 +1,69 @@
-// ref : https://itecnote.com/tecnote/c-opencv-getting-the-slider-to-update-its-position-during-video-playback/
-// ref : https://itecnote.com/tecnote/c-opencv-getting-the-slider-to-update-its-position-during-video-playback/
+// ref : https://github.com/scbaek83/Repo_OpenCV/blob/main/trackbar_book.cpp
 
-#include "opencv2/opencv.hpp"
-#include <iostream> 
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
 using namespace std; 
-using namespace cv; 
 
-int g_slider; 
-VideoCapture *g_cap = NULL; 
+int g_slider_position = 0; 
+int g_run = 1, g_dontset = 0; 
 
-void onTrackBarSlide(int current_frame, void* tmp) 
+cv::VideoCapture g_cap; 
+
+void onTrackbarSlide(int pos, void*) 
 {
-    current_frame = g_slider; 
-    g_cap->set(CAP_PROP_POS_FRAMES, current_frame);
+    if (!g_dontset) { 
+        g_run = 1; 
+        g_cap.set(cv::CAP_PROP_POS_FRAMES, pos); 
+    }
+    g_dontset = 0; 
 }
 
 int main() 
 {
-    const char* pathSrc = "./videos/seperate_layer.mp4"; 
-    g_cap = new VideoCapture(samples::findFile(pathSrc)); 
+    cv::namedWindow("ExTrackbar", cv::WINDOW_AUTOSIZE); 
+    const char* videoPath = "./videos/seperate_layer.mp4"; 
+    g_cap.open(videoPath); 
 
-    if (!g_cap->isOpened()) { 
-        printf("Can't open the video\n : %s\n", pathSrc); 
+    if (!g_cap.isOpened()) { 
+        printf("Failed to open video\n: %s\n", videoPath); 
         return -1; 
     }
 
-    int maxFrame = (int)g_cap->get(CAP_PROP_FRAME_COUNT); 
-    if (maxFrame == 0) { 
-        printf("No frames in the video\n : %S\n", pathSrc); 
-        return 0; 
-    }
+    int frames = (int)g_cap.get(cv::CAP_PROP_FRAME_COUNT); 
+    int tmpw = (int)g_cap.get(cv::CAP_PROP_FRAME_WIDTH); 
+    int tmph = (int)g_cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+    printf("Video has %d frames of dimension width: %d, height: %d\n", frames, tmpw, tmph); 
 
-    namedWindow("video"); 
-    createTrackbar("Slider", "video", &g_slider, maxFrame, onTrackBarSlide); 
+    cv::createTrackbar("Position", "ExTrackbar", &g_slider_position, frames, onTrackbarSlide); 
 
-    Mat img; 
-    while (1) { 
-        *g_cap >> img; 
-        if (img.empty()) break; 
+    cv::Mat frame; 
+    for(;;) { 
+        if (g_run != 0) {
+            g_cap >> frame; 
+            if (frame.empty()) break; 
 
-        Mat resized; 
-        resize(img, resized, Size(360, 640)); 
-        imshow("video", resized);
-        if (waitKey(25) == 27) { 
-            break; 
+            int current_pos = (int)g_cap.get(cv::CAP_PROP_POS_FRAMES); 
+            g_dontset = 1; 
+
+            cv::setTrackbarPos("Position", "ExTrackbar", current_pos); 
+            cv::imshow("ExTrackbar", frame); 
+
+            g_run -= 1; 
         }
 
+        char c = (char)cv::waitKey(10); 
+        if (c == 's') { 
+            g_run = 1; 
+            printf("Single step, run = %d\n", g_run); 
+        }
+        if (c == 'r') { 
+            g_run = -1; 
+            printf("Run mode, run = %d\n", g_run); 
+        }
+        if (c == 27) 
+            break; 
     }
-
-    delete g_cap; 
-    destroyWindow("video");
 
     return 0; 
 }
